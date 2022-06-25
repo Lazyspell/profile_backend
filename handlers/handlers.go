08b9bac0f83afbde8f278643e5d5e566d7c01056
lazyspell <Jeremy2975@yahoo.com>
2022-06-24 23:04:17 -0700

    established db connects to work with the handler created for future use

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,1 +1,23 @@
 package handlers
+
+import (
+	"github.com/lazyspell/profile_backend/driver"
+	"github.com/lazyspell/profile_backend/repository"
+	"github.com/lazyspell/profile_backend/repository/dbrepo"
+)
+
+var Repo *Repository
+
+type Repository struct {
+	DB repository.DatabaseRepo
+}
+
+func NewRepo(db *driver.DB) *Repository {
+	return &Repository{
+		DB: dbrepo.NewMongoRepo(db.NoSql),
+	}
+}
+
+func NewHandler(r *Repository) {
+	Repo = r
+}
