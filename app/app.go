94e487fa1f1f101bc21f9b98429c13c97f249b63
lazyspell <Jeremy2975@yahoo.com>
2022-06-24 23:03:06 -0700

    added http.server to the start function after the db connect was established.  updated the run function to connect to mongodb

--- app/app.go
+++ app/app.go
@@ -2,9 +2,43 @@ package app
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/lazyspell/profile_backend/driver"
+	"github.com/lazyspell/profile_backend/handlers"
 )
 
-func Start() {
+const portNumber = ":8080"
 
+func Start() {
 	fmt.Println("Application has started")
+	_, err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Starting application on http://localhost:8080"))
+
+	srv := &http.Server{
+		Addr: portNumber,
+	}
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+
+}
+
+func run() (*driver.DB, error) {
+	log.Println("Connecting to database...")
+
+	db, err := driver.ConnectMongoDB()
+	if err != nil {
+		log.Fatal("Can not connect to database! Dying...")
+	}
+
+	repo := handlers.NewRepo(db)
+	handlers.NewHandler(repo)
+
+	return db, nil
+
 }
