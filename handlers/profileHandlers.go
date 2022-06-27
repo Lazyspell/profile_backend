49919c1eda4ebf0e3980540c7e7165584aac0e57
lazyspell <Jeremy2975@yahoo.com>
2022-06-26 19:17:48 -0700

    added functionality that will send data from the db to the user

--- handlers/profileHandlers.go
+++ handlers/profileHandlers.go
@@ -1,12 +1,21 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/lazyspell/profile_backend/helpers"
 )
 
 func (m *Repository) GetAllProfiles(w http.ResponseWriter, r *http.Request) {
-	m.DB.AllProfiles()
+	profiles, err := m.DB.AllProfiles()
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(profiles)
 
 }
 
