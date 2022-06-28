cb736a72c1d983f0040bf41524f778511dd09e80
lazyspell <Jeremy2975@yahoo.com>
2022-06-27 20:56:22 -0700

    basic set up for graphql

---
+++ graph/schema.resolvers.go
@@ -0,0 +1,21 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/lazyspell/profile_backend/graph/generated"
+	"github.com/lazyspell/profile_backend/graph/model"
+)
+
+func (r *mutationResolver) CreateProfile(ctx context.Context, input model.NewProfile) (*model.Profile, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+type mutationResolver struct{ *Resolver }
