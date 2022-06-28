9da21fad430a37fb63450014aad86eeaa846e8de
lazyspell <Jeremy2975@yahoo.com>
2022-06-27 21:20:46 -0700

    added handlers logic to add a new profile

--- handlers/profileHandlers.go
+++ handlers/profileHandlers.go
@@ -2,14 +2,14 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/lazyspell/profile_backend/helpers"
+	"github.com/lazyspell/profile_backend/models"
 )
 
 func (m *Repository) GetAllProfiles(w http.ResponseWriter, r *http.Request) {
-	profiles, err := m.DB.AllProfiles()
+	profiles, err := m.DB.AllProfilesDB()
 	if err != nil {
 		helpers.ServerError(w, err)
 		return
@@ -19,7 +19,18 @@ func (m *Repository) GetAllProfiles(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (m *Repository) Working(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) InsertProfiles(w http.ResponseWriter, r *http.Request) {
+	var payload models.Profile
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		helpers.BadRequest400(w, "invalid type please check request body")
+		return
+	}
 
-	fmt.Println("working")
+	_, err = m.DB.InsertProfilesDB(payload)
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
 }
