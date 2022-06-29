2ed25e6301602fa85a118dbe25ae4551944e1ef9
lazyspell <Jeremy2975@yahoo.com>
2022-06-28 22:30:57 -0700

    added logic for functions used for the graphql queryc

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,35 +5,47 @@ package graph
 
 import (
 	"context"
-	"crypto/rand"
-	"fmt"
+	"math/rand"
+	"strconv"
 
 	"github.com/lazyspell/profile_backend/graph/generated"
 	"github.com/lazyspell/profile_backend/graph/model"
 )
 
-func (r *mutationResolver) CreateProfile(ctx context.Context, input model.NewProfile) (*model.Profile, error) {
+func (r *mutationResolver) CreateProfile(ctx context.Context, input model.NewProfile) (*model.ProfileQl, error) {
 
-	var names *model.Name
+	var names model.Name
 	names.FirstName = *input.FirstName
 	names.LastName = *input.LastName
 
-	var location *model.Location
+	var location model.Location
 	location.State = *input.State
 	location.City = *input.City
 	location.ZipCode = *input.ZipCode
 
-	profiles := model.Profile{
-		ID:          fmt.Sprintf("T%d", rand.Int()),
-		Name:        names,
-		Location:    location,
+	ProfileID := strconv.Itoa(rand.Intn(1000000)) + names.FirstName
+
+	profiles := &model.ProfileQl{
+		ID:          ProfileID,
+		Name:        &model.Name{FirstName: *input.FirstName, LastName: *input.LastName},
+		Location:    &model.Location{State: *input.State, City: *input.City, ZipCode: *input.ZipCode},
 		Email:       *input.Email,
 		Skills:      []*model.Skill{},
 		Description: *input.Description,
 	}
+	r.profile = append(r.profile, profiles)
+	return profiles, nil
+}
+
+func (r *queryResolver) Profile(ctx context.Context) ([]*model.ProfileQl, error) {
+	return r.profile, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
 type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
