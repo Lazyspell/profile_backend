e4a187329f55dbfb4c1165abc2a33ae30e59618f
lazyspell <Jeremy2975@yahoo.com>
2022-07-04 22:17:48 -0700

    added middleware to the application so the frontend could connect

---
+++ middleware/middleware.go
@@ -0,0 +1,13 @@
+package middleware
+
+import "net/http"
+
+func Cors(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// allow cross domain AJAX requests
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		next.ServeHTTP(w, r)
+	})
+}
