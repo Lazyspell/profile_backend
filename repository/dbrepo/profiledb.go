c234c6ac41386ae9886334105d613b3bd6629a07
lazyspell <Jeremy2975@yahoo.com>
2022-07-21 14:08:08 -0700

    created functionality that will help add to projects and experience arrays

--- repository/dbrepo/profiledb.go
+++ repository/dbrepo/profiledb.go
@@ -103,3 +103,33 @@ func (m *mongoDBRepo) UpdateSkillsQL(technologies model.Technologies) error {
 	}
 	return nil
 }
+
+func (m *mongoDBRepo) UpdateProjectsQL(application model.Application) error {
+	call := m.DB.Database("profile_db").Collection("profile_collection")
+
+	query := bson.M{"contact.email": "jelam2975@gmail.com"}
+	update := bson.M{"$push": bson.M{"projects": application}}
+
+	_, err := call.UpdateOne(context.TODO(), query, update)
+	if err != nil {
+		fmt.Println("failed")
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func (m *mongoDBRepo) UpdateJobQL(job model.Job) error {
+	call := m.DB.Database("profile_db").Collection("profile_collection")
+
+	query := bson.M{"contact.email": "jelam2975@gmail.com"}
+	update := bson.M{"$push": bson.M{"experience": job}}
+
+	_, err := call.UpdateOne(context.TODO(), query, update)
+	if err != nil {
+		fmt.Println("failed")
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
