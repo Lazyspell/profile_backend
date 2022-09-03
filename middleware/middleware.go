1ba636a94b956bc7aa5277bcb34a30669267240d
lazyspell <jeremy2975@yahoo.com>
2022-09-03 14:14:31 -0700

    updated cors policy

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -6,7 +6,7 @@ func Cors(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// allow cross domain AJAX requests
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 		next.ServeHTTP(w, r)
 	})
